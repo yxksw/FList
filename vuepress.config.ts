8b60229bad77efb8247935264058726bb1360476
Mao Chenyu <chenyumao815@outlook.com>
2026-03-13 18:43:38 +0800

    refactor: 重构资源挂载配置并更新网站描述和图标
    - 删除多个旧的资源挂载配置文件
    - 添加新的Github、Gitee资源挂载配置
    - 更新网站描述和favicon图标链接
    - 在.gitignore中添加.env文件

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -10,9 +10,9 @@ export default defineUserConfig({
   // 网站标题，标题颜色可在 src/client/css/main.css 中修改
   title: 'FList',
   // 网站的简介，有助于搜索引擎收录
-  description: 'FList - 将 GitHub Releases 以类似网盘的形式展示在网页上，方便用户下载开源软件。 支持视频、音频、图片、PDF 等文件的在线预览。',
+  description: 'FList - 异飨客的网络云盘。',
   // 页面 <head> 标签内添加的额外标签。 不要修改/logo.png可以替换掉这个文件，删除logo.png会导致构建出错。
-  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  head: [['link', { rel: 'icon', href: 'https://cdn.jsdmirror.com/gh/zsxcoder/github-img@main/img/favicon-yxk.avif' }]],
   // 页面预加载，所有其它页面所需的文件都会被预拉取。这对于小型站点来说是十分有帮助的，因为它会大大提升页面切换的速度。但是在你的网站有很多页面时不建议你这么做。
   // 简单来说就是，如果你的文件不多就可以打开这个选项，可以大大提高页面切换的速度，如果文件非常多就不建议打开。建议超过100个文件就不要打开这个选项。
   shouldPrefetch: true,
